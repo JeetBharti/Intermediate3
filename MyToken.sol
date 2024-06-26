3e168ffa9cf7a7be0e870a1dce27cc134aab7f65
Jeet Bharti <144922291+JeetBharti@users.noreply.github.com>
2024-06-26 11:17:44 +0530

    MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("Jeet Bharti","JBT") {
+
+        _mint(msg.sender, 1000*10**18);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
 
